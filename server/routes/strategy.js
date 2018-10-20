13affea44fcc51d13d96e23853dbe0857a2d3dc3
angad <angadsharma1016@gmail.com>
2018-10-20 15:21:49 +0530

    google oauth

--- server/routes/strategy.js
+++ server/routes/strategy.js
@@ -1,4 +1,5 @@
 const githubStrategy = require("passport-github").Strategy;
+const googleStrategy = require("passport-google-oauth20");
 const passport = require("passport");
 const {fullUserModel} = require("../schema");
 
@@ -18,6 +19,34 @@ passport.use(new githubStrategy({
     clientID:process.env.GITHUBID,
     clientSecret:process.env.GITHUBSECRET,
     callbackURL:process.env.GITHUB_CALLBACK
+},(accessToken,refreshToken,profile,done)=>{
+   // console.log(profile)
+    fullUserModel.findOne({email:profile.username})
+    .then((user)=>{
+        if(user)
+            return done(null,user);
+        let obj = new fullUserModel({
+            name:profile.displayName,
+            email:profile.email || profile.username,
+            image:profile.photos[0].value,
+            github:profile.profileUrl
+        });
+        obj.save()
+        .then((u)=>{return done(null,user);console.log("added user")})
+        .catch(console.log);
+    })
+}))
+
+
+
+
+
+
+
+passport.use(new googleStrategy({
+    clientID:process.env.GOOGLEID,
+    clientSecret:process.env.GOOGLESECRET,
+    callbackURL:process.env.GOOGLE_CALLBACK
 },(accessToken,refreshToken,profile,done)=>{
    // console.log(profile)
     fullUserModel.findOne({email:profile.username})
